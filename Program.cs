1d16f05999ca8b632f9775c70aea88452d5db318
Kirill <shadow986@mail.ru>
2023-02-28 00:00:03 +0300

    Задание 29

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// создать массив из 8 случайных чисел и вывести на экран
+int numbermas = 8;
+int[] mas = new int[numbermas];
+Random rnd = new Random();
+for (int count = 0; count < numbermas;count++)
+{
+    mas[count] = rnd.Next(0, 32654);
+}
+for (int count = 0; count < numbermas;count++)
+{
+    Console.WriteLine("Элемент массива = " + mas[count]);
+}
+
+
